342b2087e08865971089e42b30b9492a19c470bd
mrzhangfd <mrzhangfd@gmail.com>
2019-09-26 00:14:31 +0800

    done 807

--- my-leetcode-linkedlist/src/main/java/cn/sdu/icat/ListNode.java
+++ my-leetcode-linkedlist/src/main/java/cn/sdu/icat/ListNode.java
@@ -17,70 +17,10 @@ public class ListNode {
 }
 
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        /**
-         * 非递归
-         */
-        ListNode dummyNode = new ListNode(-1);
-        dummyNode.next = head;
-        ListNode p = dummyNode;
-
-        while (p.next != null && p.next.next != null) {
-            ListNode node1 = p.next;
-            ListNode node2 = node1.next;
-            ListNode next = node2.next;
-
-            node2.next = node1;
-            node1.next = next;
-            p.next = node2;
-            p = node1;
-        }
-
-        return dummyNode.next;
-        /*递归做法
-        //http://lylblog.cn/blog/4
-        //终止条件，只有一个或者没有的时候
-        if (head != null && head.next != null) {
-            //在本层中需要做的是交换head，和tHead。有三个结点 head，tHead，swapPairs（tHead.next）
-            ListNode tHead = head.next;
-            head.next=swapPairs(tHead.next);
-            tHead.next=head;
-            //给上一层的返回值是已经处理好的结点，即tHead。
-            return tHead;
-        }
-        return head;*/
-    }
-
-    public int maxIncreaseKeepingSkyline(int[][] grid) {
-        //每行最大值
-        int[] r = new int[50];
-        for (int i = 0; i < 50; i++) {
-            r[i] = 0;
-        }
-        //每列最大值
-        int[] c = new int[50];
-        for (int i = 0; i < 50; i++) {
-            c[i] = 0;
-        }
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] > c[i]) {
-                    r[i] = grid[i][j];
-                }
-            }
-        }
-
-        for (int j = 0; j < grid[0].length; j++) {
-            for (int i = 0; i < grid.length; i++) {
-                if(grid[i][j]>c[j]){
-                    c[j]=grid[i][j];
-                }
-            }
-        }
 
 
-        return 1;
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{{3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}};
     }
 }
 
